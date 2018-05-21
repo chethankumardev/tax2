3dc581e4348a1a485084f894ccad7f85d9a4fe7f
chethankumardev <39115364+chethankumardev@users.noreply.github.com>
2018-05-21 15:09:11 +0530

    upload to db

---
+++ ExportExcelToSQL/ExportExcelToSQL/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ExportExcelToSQL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Import Excel Data</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+        <!-- ADD A FILE UPLOAD CONTROL AND A BUTTON TO EXECUTE. -->
+        <div style="font:14px Verdana">
+        
+            Select a file to upload: 
+                <asp:FileUpload ID="FileUpload" Width="450px" runat="server" />
+            <p>
+                <asp:Button ID="Button1" runat="server" OnClick="ImportFromExcel" Text="ImportToDatabase" />
+            </p>
+            <p><asp:Label id="lblConfirm" runat="server"></asp:Label></p>
+                
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ ExportExcelToSQL/ExportExcelToSQL/WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExportExcelToSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+
+        }
+
+        protected void ImportFromExcel(object sender, EventArgs e)
+        {
+            // CHECK IF A FILE HAS BEEN SELECTED.
+            if ((FileUpload.HasFile))
+            {
+
+                if (!Convert.IsDBNull(FileUpload.PostedFile) &
+                    FileUpload.PostedFile.ContentLength > 0)
+                {
+
+                    //FIRST, SAVE THE SELECTED FILE IN THE ROOT DIRECTORY.
+                    FileUpload.SaveAs(Server.MapPath(".") + "\\" + FileUpload.FileName);
+
+                    SqlBulkCopy oSqlBulk = null;
+
+                    // SET A CONNECTION WITH THE EXCEL FILE.
+                    OleDbConnection myExcelConn = new OleDbConnection
+                        ("Provider=.NET Framework Data Provider for SQL Server; " +
+                            "Data Source=.\\PCMSERVER2014 "+ "\\" + FileUpload.FileName +
+                            ";Extended Properties=Excel 12.0;");
+                    try
+                    {
+                        myExcelConn.Open();
+
+                        // GET DATA FROM EXCEL SHEET.
+                        OleDbCommand objOleDB =
+                            new OleDbCommand("SELECT *FROM [Sheet1$]", myExcelConn);
+
+                        // READ THE DATA EXTRACTED FROM THE EXCEL FILE.
+                        OleDbDataReader objBulkReader = null;
+                        objBulkReader = objOleDB.ExecuteReader();
+
+                        // SET THE CONNECTION STRING.
+                        //string sCon = "Data Source=DNA;Persist Security Info=False;" +
+                        //    "Integrated Security=SSPI;" +
+                        //    "Initial Catalog=DNA_Classified;User Id=sa;Password=;" +
+                        //    "Connect Timeout=30;";
+                        string sCon = "Data Source=.\\PCMSERVER2014;Initial Catalog=taxtable;Integrated Security=True;Pooling=False";
+
+                        using (SqlConnection con = new SqlConnection(sCon))
+                        {
+                            con.Open();
+
+                            // FINALLY, LOAD DATA INTO THE DATABASE TABLE.
+                            oSqlBulk = new SqlBulkCopy(con);
+                            oSqlBulk.DestinationTableName = "EmployeeDetails"; // TABLE NAME.
+                            oSqlBulk.WriteToServer(objBulkReader);
+                        }
+
+                        lblConfirm.Text = "DATA IMPORTED SUCCESSFULLY.";
+                        lblConfirm.Attributes.Add("style", "color:green");
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        lblConfirm.Text = ex.Message;
+                        lblConfirm.Attributes.Add("style", "color:red");
+
+                    }
+                    finally
+                    {
+                        // CLEAR.
+                        oSqlBulk.Close();
+                        oSqlBulk = null;
+                        myExcelConn.Close();
+                        myExcelConn = null;
+                    }
+                }
+            }
+        }
+    }
+}
